ce8e3a4df38231c13fe11d0f8a5725ae9d43d49a
Anita Ilieva <anita.ilieva24@gmail.com>
2016-10-22 21:54:31 +0200

    Completed exercise 2

--- exercise_week5/TODO/ListViewController.swift
+++ exercise_week5/TODO/ListViewController.swift
@@ -12,11 +12,15 @@ class ListViewController: UITableViewController {
     
     var items = [String]()
     var newItem: String = ""
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         items = ["Pay bills", "Iron clothes", "Do grocery shopping"]
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "itemCell")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
 
             }
     
@@ -30,7 +34,7 @@ class ListViewController: UITableViewController {
         
         items.append(newItem)
         self.tableView.reloadData()
-
+       
     }
     
     override func didReceiveMemoryWarning() {
@@ -48,11 +52,38 @@ class ListViewController: UITableViewController {
         return items.count
     }
     
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
         cell.textLabel!.text = items[indexPath.row]
-        
         return cell
+}
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let mySelectedCell:UITableViewCell = tableView.cellForRow(at: indexPath) {
+        mySelectedCell.detailTextLabel?.textColor = UIColor.white
+        
+            if mySelectedCell.accessoryType == UITableViewCellAccessoryType.checkmark{
+        mySelectedCell.tintColor = UIColor.blue
+                        mySelectedCell.accessoryType = .none
+            }
+            else
+            {
+                mySelectedCell.accessoryType = .checkmark
+                mySelectedCell.tintColor = UIColor.blue
+
+            }
+        }
+        
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            
+        }
     }
     
 }
+
+
---
+++ exercise_week5/Todo/CollectionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  CollectionViewController.swift
+//  Todo
+//
+//  Created by Anita Ilieva on 22/10/2016.
+//  Copyright © 2016 Anita Ilieva. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+    
+    let reuseIdentifier = "reuseCell"
+    var items = [String]()
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.myLabel?.text = self.items[indexPath.item]
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+}
